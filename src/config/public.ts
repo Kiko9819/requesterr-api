3ffdbf9b63b3cfe2905bd5190a851d6cbb36d1d6
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2020-09-20 21:03:27 +0300

    Configure global config file

--- src/config/public.ts
+++ src/config/public.ts
@@ -1,1 +1,34 @@
 // will be used to export general project configurations
+import dotenv from 'dotenv';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+    // This error should crash whole process
+
+    throw new Error("Couldn't find .env file️");
+}
+
+export default {
+    /**
+     * Your favorite port
+     */
+    port: parseInt(process.env.PORT, 10),
+
+    /**
+     * Connection string
+     */
+    databaseURL: process.env.MYSQL_DB,
+
+    /**
+     * Your secret sauce
+     */
+    jwtSecret: process.env.JWT_SECRET,
+    /**
+     * API configs
+     */
+    api: {
+        prefix: '/api',
+    }
+};
