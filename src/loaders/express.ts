f1ffc6b77a35df6bb106ad241421a53f2ca06f15
Kristiyan Krastev <kristiyan@pliant.io>
2020-10-03 19:41:41 +0300

    Move code around

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,15 +1,20 @@
 import cors from 'cors';
+import express from 'express';
 import bodyParser from 'body-parser';
+import routes from '../api/public';
+import config from '../config/public';
 
 export default ({app}: { app: express.Application }) => {
+    // Load API routes
+    app.use(config.api.prefix, routes());
     /**
      * Health Check endpoints
      */
     app.get('/status', (req, res) => {
-        res.status(200).end();
+        res.status(200).send();
     });
     app.head('/status', (req, res) => {
-        res.status(200).end();
+        res.status(200).send();
     });
 
     // Useful if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
@@ -28,8 +33,7 @@ export default ({app}: { app: express.Application }) => {
 
     // Middleware that transforms the raw string of req.body into json
     app.use(bodyParser.json());
-    // Load API routes
-    // app.use(config.api.prefix, routes());
+    
 
     /// catch 404 and forward to error handler
     app.use((req, res, next) => {
