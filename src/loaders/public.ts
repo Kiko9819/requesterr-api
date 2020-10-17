efc13f2bcd86a54d6c51bfda7c9686fbd5efef24
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2020-10-18 00:59:33 +0300

    Add loader

--- src/loaders/public.ts
+++ src/loaders/public.ts
@@ -1,7 +1,10 @@
 import expressLoader from './express';
+import mysqlLoader from './mysql';
 import Logger from './logger';
 
 export default async({expressApp}) => {
+    await mysqlLoader();
+
     await expressLoader({ app: expressApp });
-    Logger.info(' Express loaded')
+    Logger.info('Express loaded')
 }
