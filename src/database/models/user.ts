996c52aa61bd160a541358a37b7878e3d8df1556
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2020-10-18 15:34:58 +0300

    Add User model

---
+++ src/database/models/user.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Sequelize } from "sequelize";
+import bcrypt from 'bcrypt';
+
+export default async (sequelize: Sequelize) => {
+    const User = sequelize.define(
+        'User',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false
+            },
+            name: {
+                type: DataTypes.STRING
+            },
+            email: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false,
+                validate: {
+                    isEmail: true
+                }
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                set(value) {
+                    const hash = bcrypt.hashSync(value, bcrypt.genSaltSync(10));
+                    this.setDataValue('password', hash);
+                }
+            }
+        }
+    );
+
+    User.prototype.validatePassword = function(password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    await User.sync({force: true});
+
+    return User;
+}
