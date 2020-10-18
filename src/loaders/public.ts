0b2ead5033b3a70cb05dd2e3b99e76d014d66fb6
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2020-10-18 15:35:54 +0300

    Add sample user create

--- src/loaders/public.ts
+++ src/loaders/public.ts
@@ -1,10 +1,19 @@
 import expressLoader from './express';
 import mysqlLoader from './mysql';
 import Logger from './logger';
+import Models from './models';
 
 export default async({expressApp}) => {
-    await mysqlLoader();
+    const sqlize = await mysqlLoader();
 
+    const models = await Models(sqlize);
+
+    models.UserModel.create({
+        name: "Ivan",
+        email: "ivan@gmail.com",
+        password: "secret"
+    });
+    
     await expressLoader({ app: expressApp });
     Logger.info('Express loaded')
 }
