cc0bb0198f8aafdaa902ed93cd88259610a7f117
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2020-11-17 00:13:43 +0200

    Define many to many relationship between user and role

--- src/loaders/models.ts
+++ src/loaders/models.ts
@@ -1,10 +1,30 @@
 import { Sequelize } from 'sequelize/types';
+import Role from '../database/models/role';
 import User from '../database/models/user';
 
 export default async (sequelize: Sequelize) => {
+    // TODO: Figure out a better way to do this
+    const setRelationsShips = (UserModel, RoleModel) => {
+        // Creates a join table between the users and the roles
+        UserModel.belongsToMany(RoleModel, {
+            through: 'UserRoles',
+            foreignKey: "userId"
+        });
+
+        RoleModel.belongsToMany(UserModel, {
+            through: 'UserRoles',
+            foreignKey: "roleId"
+        });
+    }
+
     const UserModel = await User(sequelize);
+    const RoleModel = await Role(sequelize);
+    setRelationsShips(UserModel, RoleModel);
+
+    await sequelize.sync({force: true});
 
     return {
-        UserModel
-    }
+        UserModel,
+        RoleModel
+    };
 }
