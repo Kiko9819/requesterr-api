4ba85c322bf62d2568b1cb297fe5b5de0e093ad1
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2020-11-21 14:52:16 +0200

    Add temporary solution to seeding db and setting many-to-many relationship between user and role

--- src/loaders/models.ts
+++ src/loaders/models.ts
@@ -4,24 +4,41 @@ import User from '../database/models/user';
 
 export default async (sequelize: Sequelize) => {
     // TODO: Figure out a better way to do this
-    const setRelationsShips = (UserModel, RoleModel) => {
+    const setRelationsShips = async (UserModel, RoleModel) => {
         // Creates a join table between the users and the roles
-        UserModel.belongsToMany(RoleModel, {
+        await UserModel.belongsToMany(RoleModel, {
             through: 'UserRoles',
             foreignKey: "userId"
         });
 
-        RoleModel.belongsToMany(UserModel, {
+        await RoleModel.belongsToMany(UserModel, {
             through: 'UserRoles',
             foreignKey: "roleId"
+        });
+    }
+    // TODO: use sequelizes seeders
+    const seedDb = async (UserModel, RoleModel) => {
+        await RoleModel.create({
+            name: "ADMIN",
+            description: "Is god like"
+        })
+        await RoleModel.create({
+            name: "USER",
+            description: "Is not so god like"
+        })
+        await UserModel.create({
+            name: "admin",
+            email: "admin@admin.com",
+            password: "admin"
         });
     }
 
     const UserModel = await User(sequelize);
     const RoleModel = await Role(sequelize);
-    setRelationsShips(UserModel, RoleModel);
+    await setRelationsShips(UserModel, RoleModel);
 
-    await sequelize.sync({force: true});
+    await sequelize.sync({ force: true });
+    await seedDb(UserModel, RoleModel);
 
     return {
         UserModel,
