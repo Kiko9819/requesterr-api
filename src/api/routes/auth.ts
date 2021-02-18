249d6a2b58cf3f270c8fe8daa5ad66d108ea7bf9
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-02-18 23:48:22 +0200

    Have he api return proper results

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -7,6 +7,8 @@ import AuthService from '../../services/auth';
 
 const route = Router();
 
+// TODO: finish up adding the middleware for isAuth where needed
+// also think of how you are going to add redis to the whole picture
 export default (app: Router) => {
     app.use('/auth', route);
 
@@ -29,20 +31,37 @@ export default (app: Router) => {
         }
     });
 
+    // find a solution to tokens that have not yet expired but the user is logged out...
+    route.post("/signout", async (req: Request, res: Response, next: NextFunction) => {
+        const logger: Logger = Container.get('logger');
+        logger.debug('Calling Sign-Out endpoint with body: %o', req.body);
+
+        return res.status(200).json({message: "Okay, I'm not gonna do anything as of now though..."});
+    })
+
     route.post('/signin', async (req: Request, res: Response, next: NextFunction) => {
         const logger: Logger = Container.get('logger');
         logger.debug('Calling Sign-In endpoint with body: %o', req.body);
 
         try {
             const authService = Container.get(AuthService);
-            const { user, token, status } = await authService.SignIn(req.body as IUserLoginDTO);
+            const { user, access_token, refresh_token, status } = await authService.SignIn(req.body as IUserLoginDTO);
 
             if (status === 400) {
-                return res.status(status).json({ status: status, message: "Username or password is incorrect" });
+                return res.status(status).json({ 
+                    status: status, 
+                    message: "Username or password is incorrect"
+                });
             }
 
             if (status === 200) {
-                return res.status(status).json({ status: status, message: "Successfully logged in", user: user, token: token });
+                return res.status(status).json({ 
+                    status: status, 
+                    message: "Successfully logged in", 
+                    user: user, 
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                });
             }
 
             return res.status(status).json({ status: status, message: "User doesn't exist" });
