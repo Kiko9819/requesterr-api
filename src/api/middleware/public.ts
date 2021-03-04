5a5f82ea358469440ed7dee0665aadbe4eef0c7d
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-03-04 23:16:42 +0200

    Expose attach current user

--- src/api/middleware/public.ts
+++ src/api/middleware/public.ts
@@ -1,5 +1,7 @@
 import isAuth from './isAuth';
+import attachCurrentUser from './attachUser';
 // will be use to expose middleware
 export default {
-    isAuth
+    isAuth,
+    attachCurrentUser
 }
