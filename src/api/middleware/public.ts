04cad001941741339d6b6ca5b00888e03c564dd8
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-03-06 16:04:50 +0200

    Expose checkSecret middleware

--- src/api/middleware/public.ts
+++ src/api/middleware/public.ts
@@ -1,7 +1,9 @@
 import isAuth from './isAuth';
 import attachCurrentUser from './attachUser';
+import checkSecret from './checkSecret';
 // will be use to expose middleware
 export default {
     isAuth,
-    attachCurrentUser
+    attachCurrentUser,
+    checkSecret
 }
