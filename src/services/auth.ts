5a35c44f33ebd06bc41003f42f5c40bd99598b41
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-03-06 16:06:23 +0200

    Remove comments

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -100,20 +100,13 @@ export default class AuthService {
         }
     }
 
-    private generateRefreshToken(user) {
-        // //get all user's refresh tokens from DB
-        // const userRefreshTokens = mockDB.tokens.filter(token => token.userId === payload.id); // check if there are 5 or more refresh tokens,
-        // // which have already been generated. In this case, we should
-        // // remove all this refresh tokens and leave only new one for security reason
-        // if (userRefreshTokens.length >= 5) {
-        //     mockDB.tokens = mockDB.tokens.filter(token => token.userId !== payload.id);
-        // } const refreshToken = jwt.sign({ user: payload }, jwtSecretString, { expiresIn: '30d' }); mockDB.tokens.push({
-        //     id: uuidv1(),
-        //     userId: payload.id,
-        //     refreshToken
-        // }); return refreshToken;
+    // public async RefreshToken(): Promise<any> {
+    //     return {
+    //         access_token: this.generateJWT(15)
+    //     }
+    // }
 
-        // temp solution
+    private generateRefreshToken(user) {
         return this.generateJWT(user, 30);
     }
 
